bae0269551ac61170e7358ea188e4451a75707d3
Asher Lien <asher.lien@riversoft.com.tw>
2024-07-12 16:30:42 +0800

    西恩

--- app.ts
+++ app.ts
@@ -1,4 +1,37 @@
-import Server from './models/server'
+import express, { Application } from 'express'
+import config from './config'
+import departmentsRoutes from './routes/departments'
+import cors from 'cors'
+
+class Server {
+  private app: Application
+  
+  constructor() {
+    this.app = express()
+    this.middlewares()
+    this.routes()
+  }
+  
+  middlewares() {
+    this.app.use(cors())
+    this.app.use(express.json())
+    this.app.use(express.static('public'))
+  }
+
+  routes() {
+    this.app.use('/api/departments', departmentsRoutes)
+  }
+
+  listen() {
+    this.app.listen(config.port, () => {
+      // eslint-disable-next-line no-console
+      console.log(`Server up and running at port: ${config.port}`)
+    })
+  }
+
+}
+
+export default Server
 
 const server = new Server()
 
