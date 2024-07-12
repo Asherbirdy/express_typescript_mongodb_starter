1beb9562162b538b68358a2561c7242ab6bf5262
Asher Lien <asher.lien@riversoft.com.tw>
2024-07-12 16:13:08 +0800

    增加增加hotreload

--- controllers/departments.ts
+++ controllers/departments.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express'
 
 export const getDepartments = async (req: Request, res: Response) => {
   res.json({
-    msg: 'test'
+    msg: 'tessaaaaasssssssasdassssssst'
 
   })
 
