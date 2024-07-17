7606f9acf1e85d554e8601b8d3815225f0575659
Asher Lien <asher.lien@riversoft.com.tw>
2024-07-17 11:50:51 +0800

    e/ vup

--- controllers/DevController.ts
+++ controllers/DevController.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from 'express'
+import { StatusCode } from '../enums'
 
 export const DevController = {
 
   // ** 
   get: (req: Request, res: Response) => {
-    res.json( {
+    res.status(StatusCode.OK).json( {
       msg: 'DevController_GET'
     } )
   }
