99f59e09e165863b72465f75a7658b338081b1b0
Asher Lien <asher.lien@riversoft.com.tw>
2024-07-29 09:48:02 +0800

    更新

--- controllers/DevController.ts
+++ controllers/DevController.ts
@@ -6,6 +6,7 @@ export const DevController = {
 
   // ** 
   get: (req: Request, res: Response) => {
+    console.log(config.environment)
     res.status(StatusCode.OK).json( {
       msg: 'DevController_GET',
       envPort: config.environment
