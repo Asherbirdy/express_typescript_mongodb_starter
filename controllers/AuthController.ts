d249089dc73134ef1eb5c3d710a661131283b38f
Asher Lien <asher.lien@riversoft.com.tw>
2024-07-29 11:36:19 +0800

    更新api

--- controllers/AuthController.ts
+++ controllers/AuthController.ts
@@ -5,5 +5,16 @@ export const AuthController = {
   // ** 
   register: (req: Request, res: Response) => {
     res.status(StatusCode.CREATED).json({ msg: 'register' })
-  }
+  },
+
+  // ** 
+  login: (req: Request, res: Response) => {
+    res.status(StatusCode.CREATED).json({ msg: 'login' })
+  },
+
+  // ** 
+  logout: (req: Request, res: Response) => {
+    res.status(StatusCode.CREATED).json({ msg: 'logout' })
+  },
+
 }
--- routes/AuthRoutes.ts
+++ routes/AuthRoutes.ts
@@ -4,5 +4,7 @@ import { AuthController } from '../controllers'
 const router = Router()
 
 router.post( '/register', AuthController.register)
+router.post('/login', AuthController.login)
+// router.delete('/logout', authenticateUser, AuthController.logout);
 
 export default router
